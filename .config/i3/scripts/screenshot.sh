7423ee363edde99733800f8b6dce81da8ac201d1
karivolas <tsimpidisd@gmail.com>
2021-02-03 20:39:28 +0200

    [i3] Use maim instead of scrot for screenshots

--- .config/i3/scripts/screenshot.sh
+++ .config/i3/scripts/screenshot.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
-# Check is scrot exist
-if ! which scrot &> /dev/null; then
-  echo "scrot not found"
+# Check is maim exist
+if ! which maim &> /dev/null; then
+  echo "maim not found"
   exit 1;
 fi
 
-# Use dmenu to get user selection
-SEL=$(echo -e "1: Select window or rectange\n2: Whole Screen" | dmenu -l 10)
-
-# Select region or window.
-# User should either click on a window, or drag a rectange on the screen
-if [[ $SEL == 1:* ]]; then
-  OPTIONS='-s'
-# Grab all monitors
-elif [[ $SEL = 2:* ]]; then
-  OPTIONS=''
-else
-  exit 1
-fi;
-
-# Take screenshot, save in `/tmp` and copy file path in system clipboad
-scrot '/tmp/%Y-%m-%d--%H-%M-%S--$wx$h-scrot.png' $OPTIONS -e 'echo "$f" | xclip -sel clip; feh $f'
+FILENAME=/tmp/$(date '+%Y-%m-%d--%H-%M-%S').png;
+# Take screenshot
+maim -suo $FILENAME
+# Copy file path to clipboard
+echo $FILENAME | xclip -sel clip;
+# Open file
+feh $FILENAME
